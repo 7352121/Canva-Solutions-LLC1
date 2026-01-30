ff66f34c8336a838932c6c0d4d1fdb9042a6a821
Hamza <htariq2050@gmail.com>
2026-01-30 14:38:17 +0500

    Remove react-helmet-async and update meta management

--- src/components/common/MetaData.jsx
+++ src/components/common/MetaData.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
 import { useLocation } from 'react-router-dom';
 import { SITE_URL } from '../../utils/config';
 
@@ -8,19 +7,19 @@ const MetaData = ({ title, description, keywords }) => {
     const currentUrl = `${SITE_URL}${location.pathname}`;
 
     return (
-        <Helmet>
-            {/* Standard Tags */}
+        <>
+            {/* React 19 hoists these to <head> automatically */}
             <title>{title}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
             <link rel="canonical" href={currentUrl} />
 
-            {/* Open Graph Tags (Facebook/LinkedIn) */}
+            {/* Open Graph Tags */}
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:url" content={currentUrl} />
             <meta property="og:type" content="website" />
-        </Helmet>
+        </>
     );
 };
 
